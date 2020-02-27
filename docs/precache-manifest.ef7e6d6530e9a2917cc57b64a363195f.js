5fd26e9b8597b2e6b42bece74baee8ccad9f0026
Boda Zsolt <Boda.Zsolt@outlook.com>
2020-02-27 20:14:42 +0100

    demo

---
+++ docs/precache-manifest.ef7e6d6530e9a2917cc57b64a363195f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c93a296c1f415a3bfd5b1317a922d13a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3749376d304109afe629",
+    "url": "/static/css/main.c421cb72.chunk.css"
+  },
+  {
+    "revision": "7d8e77af9f26b3c186e1",
+    "url": "/static/js/2.217997a8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.217997a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3749376d304109afe629",
+    "url": "/static/js/main.d4dc80e0.chunk.js"
+  },
+  {
+    "revision": "7f35b828328a0e8bd64b",
+    "url": "/static/js/runtime-main.06e25118.js"
+  }
+]);
